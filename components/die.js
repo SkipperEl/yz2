966ff97a9df9ddea9da84d7aca3fb6711d8ed554
Elliott Noel <github@miu.ca>
2020-03-21 19:34:56 -0400

    add die icon

--- components/die.js
+++ components/die.js
@@ -1,14 +1,35 @@
+
+const containerStyle = {
+  width: "75px",
+  height: "100px",
+  display: "flex",
+  flexDirection: "column",
+  alignItems: "center"
+};
+
 const imgStyle = {
   width: "75px",
   height: "75px"
 };
 
+const lockStyle = {
+  width: "35px",
+  height: "35px",
+  marginTop: "-18px"
+};
+
 const Die = props => (
-  <img
-    src={`/die_${props.value}.png`}
-    style={imgStyle}
-    onClick={props.onClick}
-  />
+  <div style={containerStyle}>
+    <img
+      src={`/die_${props.value}.png`}
+      style={imgStyle}
+      onClick={props.onClick}
+    />
+    <img
+      src="/lock.png"
+      style={lockStyle}
+    />
+  </div>
 );
 
 export default Die;
