87566a0d3181518c8b12f179407f2e52c0230138
Elliott Noel <github@miu.ca>
2020-03-21 13:19:34 -0400

    sized

--- components/die.js
+++ components/die.js
@@ -1,9 +1,10 @@
 const imgStyle = {
-
+  width: "75px",
+  height: "75px"
 };
 
 const Die = () => (
-  <img src="/die_1.png" />
+  <img src="/die_1.png" style={imgStyle} />
 );
 
 export default Die;
