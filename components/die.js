3d0481c0531bb5280498876106b775f8447677a1
Elliott Noel <github@miu.ca>
2020-03-21 13:17:55 -0400

    dumb die component

---
+++ components/die.js
@@ -0,0 +1,9 @@
+const imgStyle = {
+
+};
+
+const Die = () => (
+  <img src="/die_1.png" />
+);
+
+export default Die;
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,10 @@
+import Die from "../components/die";
+
 export default function Index() {
   return (
     <div>
       <p>Hello Next.js</p>
+      <Die />
     </div>
   );
 }
